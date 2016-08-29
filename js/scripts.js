cdafe45a77206bf9994346957b6c6c71c81b6c58
Ian Baum and Rebecca Allen <ian@>
2016-08-29 11:04:06 -0400

    add most of the variables for the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 var Player = function(){
   this.playerName = playerName;
-  this.maxHealth = 10;
+  this.maxHealth = maxHealth;
   this.currentHealth = this.maxHealth;
   this.damage = damage;
   this.inventory = [];
@@ -18,7 +18,33 @@ var Room = function(){
   this.usable = [];
 }
 
+var Loot = function(){
+  this.inventory = false;
+  this.equipped = false;
+  this.info = "";
+}
 
+var Weapon = function(){
+  this.inventory = false;
+  this.equipped = false;
+  this.info = "";
+  this.damage = damage;
+}
+
+var NPC = function(){
+  this.hostile = false;
+  this.info = "";
+  this.maxHealth = maxHealth;
+  this.currentHealth = this.maxHealth;
+  this.damage = damage;
+  this.dialogue = dialogue;
+}
+
+var Usable = function(){
+  this.active = false;
+  this.door = false;
+  this.info = [];
+}
 
 
 
