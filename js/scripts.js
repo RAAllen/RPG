b3388c4f7ea8b55787ccf6079cfd5f9e9adc0a55
Ian Baum and Rebecca Allen <rebecca@>
2016-08-29 10:40:26 -0400

    set up basic index.html and project directories, etc. and start scripts.js variables

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//business logic
+
+var Player = function(){
+  this.playerName = playerName;
+  this.maxHealth = 10;
+  this.currentHealth = this.maxHealth;
+  this.damage = damage;
+  this.inventory = [];
+  this.info = info;
+}
+
+var Room = function(){
+  this.active = false;
+  this.info = [];
+  this.loot = [];
+  this.characters = [];
+  this.doors = [];
+  this.usable = [];
+}
+
+
+
+
+
+
+
+//user logic
