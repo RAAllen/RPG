d14fc222e08bd59c97dd672f952f2df6e96fdc7f
Ian Baum and Rebecca Allen <ian@>
2016-08-29 13:03:07 -0400

    add minor css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //business logic
+var commands = ["LOOK", "USE", "ATTACK", "GET", "TALK", "EQUIP", "DROP"];
 
-var Player = function(){
-  this.playerName = playerName;
+var Player = function(playerName,maxHealth,damage,info){
+  this.name = playerName;
   this.maxHealth = maxHealth;
   this.currentHealth = this.maxHealth;
   this.damage = damage;
@@ -18,10 +19,11 @@ var Room = function(){
   this.usable = [];
 }
 
-var Loot = function(){
+var Loot = function(name,info){
+  this.name = name;
   this.inventory = false;
   this.equipped = false;
-  this.info = "";
+  this.info = info;
 }
 
 var Weapon = function(){
@@ -51,3 +53,35 @@ var Usable = function(){
 
 
 //user logic
+
+$(document).ready(function(){
+
+  var player = new Player("",10,1,"It's you.");
+  var linenClothes = new Loot("Linen Clothes","It's your clothes");
+  player.inventory.push(linenClothes);
+
+  $("#userInfo").submit(function(event){
+    event.preventDefault();
+
+    var name = $("#userName").val();
+    player.name = name;
+
+    console.log(player);
+
+
+  });
+  $("#userEntries").submit(function(event){
+    event.preventDefault();
+    var userEntry = $("#userInput").val();
+    for(i=0;i<commands.length;i++){
+      if(userEntry === commands[i]){
+        $("#story").append("<li>> " + userEntry + "</li>");
+        $("#userInput").val("");
+      }
+    }
+      // $("#story").append("<li>I don't know the command '" + userEntry + "'</li>");
+      // $("#userInput").val("");
+
+
+  });
+});
